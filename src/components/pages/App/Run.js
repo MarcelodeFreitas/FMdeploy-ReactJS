98aaab13b96a12aa2a740c2748f71cfdd3e29a76
MarcelodeFreitas <55439529+MarcelodeFreitas@users.noreply.github.com>
2021-09-08 17:24:07 +0100

    king on run functionality for ob masks

--- src/components/pages/App/Run.js
+++ src/components/pages/App/Run.js
@@ -4,59 +4,170 @@ import "./Main.css"
 import "./Run.css"
 import AppHeader from "../../AppHeader"
 import { useDropzone } from "react-dropzone"
-import { useState } from "react"
+import { useState, useContext } from "react"
+import axios from "axios"
+import baseUrl from "../../server/server"
+import StoreContext from '../../Store/Context'
+import { CircularProgress, Button, ThemeProvider } from '@material-ui/core'
+import theme from "../../theme/theme"
 
 const Run = (props) => {
-  console.log(props.location.state)
 
-  
+  const { token } = useContext(StoreContext)
+
+  const [inputFileId, setInputFileID] = useState("")
+
+  const [outputFile, setOutputFile] = useState("")
+
+  const [outputFileName, setOutputFileName] = useState("")
+
+  const [downloadLink, setDownloadLink] = useState("")
+
+  const [isRunning, setIsRunning] = useState(false)
+
+  /* console.log(props.location.state) */
+
+  //upload input file to the server and get the id
+  const uploadInputFile = async (token, inputFile) => {
+    try {
+      const formData = new FormData()
+      formData.append('input_file', inputFile)
+      const response = await axios.post(`${baseUrl}/files/inputfile`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          Authorization: `Bearer ${token}`
+        }
+      })
+      console.log(await response)
+      console.log(await response.data)
+      setInputFileID(await response.data.input_file_id)
+      return await response.data.input_file_id
+    } catch (e) {
+      console.log(e)
+      console.log("uploadInputFile error: ", e.response)
+    }
+  }
+
+  //run model providing input file id and ai id
+  const runAi = async (token, aiId, fileId) => {
+    console.log(aiId, fileId)
+    try {
+      const response = await axios.post(`${baseUrl}/ai/run`, {
+        ai_id: aiId,
+        input_file_id: fileId,
+      }, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        responseType: 'arraybuffer', //prevents the file from corrupting
+      })
+      console.log(await response)
+
+      const filename = await response.headers['content-disposition'].split('filename=')[1].split(';')[0]
+
+      setOutputFileName(await filename)
+
+      const url = window.URL.createObjectURL(
+        new Blob([await response.data], {
+          type: await response.headers['content-type']
+        })
+      )
+
+      const link = document.createElement('a')
+      link.href = url
+      link.setAttribute('download', filename)
+      document.body.appendChild(link)
+      setDownloadLink(link)
+
+    } catch (e) {
+      console.log(e)
+      console.log("runAi error: ", e.response)
+    }
+  }
+
+  const handleRun = async (token, inputFile, aiId) => {
+    if (inputFile) {
+      console.log(inputFile)
+      setIsRunning(true)
+      const fileId = await uploadInputFile(token, inputFile)
+      await runAi(token, aiId, await fileId)
+      setIsRunning(false)
+    } else {
+      console.log(inputFile)
+    }
+    
+  }
 
   const InputFilesDropzone = () => {
 
     const [files, setFiles] = useState([])
 
-    const clearFiles = () => {
-      setFiles([])
-    }
-
     const getFiles = () => {
       console.log(files)
     }
 
-    const { getRootProps, getInputProps } = useDropzone({
+
+    const { getRootProps, getInputProps, acceptedFiles } = useDropzone({
       disabled: false,
-      /* validator: modelFilesValidator, */
+      maxFiles: 1,
+      onDrop: () => {setOutputFileName("")},
       onDropAccepted: acceptedFiles => {
-        setFiles(acceptedFiles.map(file => Object.assign(file)))
-        console.log(files)
+        setOutputFileName("")
+        setFiles(acceptedFiles[0])
+        console.log("files: ", files)
       }
     })
 
-    const fileList = files.map((file => (
-      <li className="file-list-errors" key={file.name}>
-        {file.name} - {file.size} bytes
-      </li>
-    ))
-    )
+    const clearFiles = () => {
+      setFiles([])
+      acceptedFiles.splice([], 1)
+      setInputFileID("")
+      setOutputFileName("")
+    }
 
     return (
       <div className="run-box">
+        <h1 className="run-labels">1. INPUT</h1>
         <div {...getRootProps()} className="run-dropzone">
           <input {...getInputProps()} />
-          <p>Drop Files Here</p>
-          <p>- or -</p>
-          <p>Click to Upload</p>
-        </div>
-        <div>
-          <ul className="file-list-errors">{fileList}</ul>
+          {acceptedFiles.length === 0 ?
+            <div className="center">
+              <p>Drop File Here</p>
+              <p>- or -</p>
+              <p>Click to Upload</p>
+              <div className="blank-line" />
+              <p className="content-box-text-small">(you can only drop 1 file here)</p>
+            </div>
+            :
+            <p className="fileName">{acceptedFiles[0].name}</p>
+          }
         </div>
         <div className="run-buttons">
           <div className="clear-button-fat" onClick={clearFiles}>
             CLEAR
           </div>
-          <div className="submit-button" onClick={getFiles}>
-            SUBMIT
-          </div>
+          {console.log(isRunning)}
+          {/* {acceptedFiles.length === 0 ?
+            <div className="submit-button-disabled" onClick={() => { console.log("disabled") }}>
+              SUBMIT
+            </div>
+            :
+            <div className="submit-button" onClick={() => handleRun(token, acceptedFiles[0], props.location.state.ai_id)}>
+              SUBMIT
+            </div>
+          } */}
+          <Button
+              style={{width: '48%', fontSize: "15px", fontWeight: "bold"}}
+              startIcon={isRunning ? <CircularProgress size="1rem" /> : null}
+              disabled={isRunning}
+              variant="contained"
+              color="primary"
+              type="submit"
+              onClick={() =>  handleRun(token, acceptedFiles[0], props.location.state.ai_id)}
+          >
+              Submit
+          </Button>
+          
         </div>
       </div>
     )
@@ -64,21 +175,43 @@ const Run = (props) => {
 
   return (
     <>
+    <ThemeProvider theme={theme}>
       <Sidebar />
       <div className="main">
-        <AppHeader title={props.location.state.title} button="BACK" buttonIcon="" path="/my"/>
+        <AppHeader title={props.location.state.title} button="BACK" buttonIcon="" path="/my" />
         <div className="run-container">
+          <div style={{ marginBottom: "40px", marginTop: "40px" }}>
+            <div>ai id: {props.location.state.ai_id}</div>
+            <div>input type: {props.location.state.input_type}</div>
+            <div>is Private: {props.location.state.is_private.toString()}</div>
+          </div>
           <div className="description">
-            {props.location.state.description} askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh 
+            {props.location.state.description} askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh  askdjhf alksdjh laskdjh laskdj halksjdh lkasjd hlaskjh
           </div>
           <div className="run-boxes">
-            <InputFilesDropzone/>
-            <InputFilesDropzone/>
+            <InputFilesDropzone />
+            <div className="run-box">
+              <h1 className="run-labels">2. OUTPUT</h1>
+              <div className="run-dropzone">
+                <div className="center">
+                  {outputFileName && <p className="run-fileName" onClick={() => downloadLink.click()}>{outputFileName}</p>}
+                </div>
+              </div>
+              {outputFileName &&
+                <div className="run-buttons">
+                  <div className="clear-button-fat" onClick={() => { }}>
+                    SHARE
+                  </div>
+                  <div className="submit-button" onClick={() => downloadLink.click()}>
+                    DOWNLOAD
+                  </div>
+                </div>
+              }
+            </div>
           </div>
         </div>
-        
-        
       </div>
+      </ThemeProvider>
     </>
   )
 }
